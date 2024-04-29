cce632680faaa71098a8bd2c6a9c1fb52f6fdfc3
yoyo383 <yoad.winter@gmail.com>
2024-04-29 17:44:24 +0300

    added button stuff

--- Game/src/Button.cpp
+++ Game/src/Button.cpp
@@ -1,7 +1,7 @@
 #include "Button.hpp"
 #include "graphics.hpp"
 
-Button::Button(sf::Vector2f position, graphics::TextureManager& textures, std::string normalID, std::string clickedID)
+Button::Button(sf::Vector2f position, TextureManager& textures, std::string normalID, std::string clickedID)
 	: normalSprite(textures[normalID]),
 	clickedSprite(textures[clickedID]),
 	currentSprite(&normalSprite) 
@@ -11,12 +11,17 @@ Button::Button(sf::Vector2f position, graphics::TextureManager& textures, std::s
 	isClicked = false;
 }
 
+void Button::setSizeRelativeToWindow(sf::RenderWindow& window, float percentage) {
+	float scale = window.getSize().x / (1 / percentage * currentSprite->getGlobalBounds().width);
+	normalSprite.setScale(scale, scale);
+	clickedSprite.setScale(scale, scale);
+}
+
 bool Button::isButtonClicked(sf::RenderWindow& window) {
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 		sf::Vector2i mousePos = sf::Mouse::getPosition(window);
 		sf::FloatRect buttonRect = currentSprite->getGlobalBounds();
-		isClicked = mousePos.x > buttonRect.left && mousePos.x < buttonRect.left + buttonRect.width
-			&& mousePos.y > buttonRect.top && mousePos.y < buttonRect.top + buttonRect.height;
+		isClicked = buttonRect.contains(mousePos.x, mousePos.y);
 	}
 	else
 		isClicked = false;
--- Game/src/Button.hpp
+++ Game/src/Button.hpp
@@ -10,7 +10,10 @@ private:
 	bool isClicked;
 
 public:
-	Button(sf::Vector2f position, graphics::TextureManager& textures, std::string normalID, std::string clickedID);
+	Button(sf::Vector2f position, TextureManager& textures, std::string normalID, std::string clickedID);
+
+	void setSizeRelativeToWindow(sf::RenderWindow& window, float percentage);
+
 	bool isButtonClicked(sf::RenderWindow& window);
 	void draw(sf::RenderWindow& window) const;
 };
