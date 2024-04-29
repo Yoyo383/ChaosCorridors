0c9ca16e60db18c9bca3328c31ff0071a8c5f386
yoyo383 <yoad.winter@gmail.com>
2024-04-29 18:28:00 +0300

    removed unnecessary functions

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -7,22 +7,12 @@
 
 constexpr unsigned short PORT = 12345;
 
-static sf::Vector2f wasdInput() {
-	return {
-		(float)(sf::Keyboard::isKeyPressed(sf::Keyboard::W) - sf::Keyboard::isKeyPressed(sf::Keyboard::S)),
-		(float)(sf::Keyboard::isKeyPressed(sf::Keyboard::D) - sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-	};
-}
-
-static void resetMousePos(sf::RenderWindow& window, sf::Vector2i fixedMousePos) {
-	sf::Mouse::setPosition(fixedMousePos, window);
-}
-
 int main() {
 	srand(time(NULL));
 	sockets::initialize();
 
 	sf::RenderWindow window(sf::VideoMode(1000, 800), "Yay window!");
+
 	TextureManager textures;
 	textures.addTexture("floor", "assets/wood.png");
 	textures["floor"].setRepeated(true);
